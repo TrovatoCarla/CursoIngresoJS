7e1cfd74a68cad1cfc4fafd1704170ee15b3f135
alumno <alumno>
2019-02-06 20:25:51 -0300

    Ejercicio 5 terminado

--- 1-EntradaSalida/jsEntradaSalida-5.js
+++ 1-EntradaSalida/jsEntradaSalida-5.js
@@ -2,6 +2,15 @@
 ej.: "Usted se llama José y tiene 66 años" 	*/
 function mostrar()
 {	
-	
+	var nombre;
+	var edad;
+	edad=laEdad.value;
+	nombre=elNombre.value;
+
+    //alert(nombre+edad);
+    alert("Usted se llama" +nombre+ "y tiene" +edad+ "años");
+
+
+
 }
 
--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -3,6 +3,24 @@ transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
+var primernumero;
+var segundonumero;
+var suma;
+
+primernumero=numeroUno.value;
+segundonumero=numeroDos.value;
+//alert(primernumero+segundonumero);
+
+primernumero=parseInt(primernumero);
+segundonumero=parseInt(segundonumero);
+
+suma=primernumero+segundonumero;
+
+
+alert("La suma es: " +suma);
+
+
+
 
 }
 
