02b8be4d272e9e81e8d6274aab3622a9a4b1f62d
alumno <alumno>
2019-02-11 21:29:28 -0300

    InstIF 6

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,8 +1,38 @@
 function mostrar()
 {
 //tomo la edad  
+	var suEdad;
 
+	suEdad=edad.value;
 
+	suEdad=parseInt(suEdad);
+/*
+	if (suEdad>18)
+	{
+		alert("Usted es mayor de edad");
+	}
+	if (suEdad<13)
+	{
+		alert("Usted es un niño");
+	}
+	else
+	{
+		alert("Usted es un adolescente");
+		}
+*/
 
+if(suEdad>18)
+{
+	alert("Usted es mayor de edad");
+}
+else
+	if(suEdad<13)
+{
+	alert("Usted es menor de edad");
+}
+else
+	{
+		alert("Usted es adolescente");
+}
 
 }//FIN DE LA FUNCIÓN
