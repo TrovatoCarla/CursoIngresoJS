f07925e53c3ae33e10bd629bb5637ec263a27f6e
alumno <alumno>
2019-02-11 22:14:31 -0300

    InstIF

--- 2-InstruccionIf/jsInstruccionIF (7).js
+++ 2-InstruccionIf/jsInstruccionIF (7).js
@@ -1,7 +1,24 @@
 function mostrar()
 {
 //tomo la edad  
+var suEdad;
+var estCivil;
 
+suEdad=edad.value;
+estCivil=estadoCivil.value;
+
+suEdad=parseInt(suEdad);
+
+if(suEdad<18 && estCivil==casado);
+{
+	alert("Es muy pequeño para NO ser soltero");
+	}
+	else
+		if(estadoCivil==divorciado)
+			{
+	alert("Es muy pequeño para NO ser soltero");
+	}
+	
 	
 
 
