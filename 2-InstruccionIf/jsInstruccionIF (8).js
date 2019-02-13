81257bf22ec2e9a565433574e9c94e76245130eb
alumno <cntrovato@buenosaires.gob.ar>
2019-02-13 18:01:54 -0300

    Random

--- 2-InstruccionIf/jsInstruccionIF (8).js
+++ 2-InstruccionIf/jsInstruccionIF (8).js
@@ -2,7 +2,23 @@ function mostrar()
 {
 //tomo la edad  
 
+	var suEdad;
+	var eCivil;
+
+	eCivil=estadoCivil.value;
+	suEdad=edad.value;
+
+	suEdad=parseInt(suEdad);
+
+
+	if(suEdad>=18 && eCivil==soltero)
+	{
+		alert("Es soltero y no es menor");
+		}
 	
+
+
+
 
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -1,6 +1,12 @@
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
+	var numeroRam;
+
+
+	numeroRam=Math.floor(Math.random() * 10) + 1; 
+
+	alert(numeroRam);
+
 
 }//FIN DE LA FUNCIÓN
