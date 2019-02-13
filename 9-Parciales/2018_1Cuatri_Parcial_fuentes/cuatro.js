ec98cd4090408459ee1adfcbefb065d99d91f84b
alumno <alumno>
2019-02-13 20:38:14 -0300

    Ejer hoy

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
@@ -1,25 +1,33 @@
-function mostrar()
+// al realizar una compra, si compra de mas de dos productos se realiza un descuento de 10% y si supera los $ 2000 se agrega descuento adicional de 15%, si el pago es con tarjeta y no efectivo al precio final se le agrega un 10% de recargo
 {
-var numeroUno;
-var numeroDos;
+	//defino variables
+	var cantidad;
+	var importe;
+	var tipoDePago;
+	
+	// tomo los datos
+	cantidad= prompt("ingrese cantidad");
+	importe= prompt("ingrese importe");	
+	tipo= prompt("ingrese tipo de pago 1- efete 2- tarjeta");
 
+	// parseint para los necesarios
+	cantidad= parseInt(cantidad);
+	importe= parseInt(importe);
 
-numeroUno=prompt("Indica un número");
-numeroDos=prompt("Indica el segundo número");
-
-
-if (numeroUno==numeroDos)
-{
-	alert(numeroUno+numeroDos);
-}
-else
-	if(numeroUno>numeroDos)
+	if(productos>2)
+	{
+		importe = importe*0.9;
+		if(importe>2000)
 		{
-			alert(numeroUno-numeroDos);
-			}
-			else
-				{
-				alert(numeroUno+numeroDos);
-			}
-				
+			importe = importe*0.85;
+		}
+	}	
+	
+	if(tipoDePago=="2")
+	{
+		importe=importe*1.1;
+	}	
+	alert( "el importe es"+ importe);
+
+
 }
