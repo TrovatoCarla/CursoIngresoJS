fc2243af2aac4821ba6f05681280b64b2ff4f233
alumno <cntrovato@buenosaires.gob.ar>
2019-02-13 16:53:49 -0300

    ejerc 10

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,19 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
+	var importeUno;
+	var importeConAumento;
+	var aumento;
+
+
+	importeUno=document.getElementById('importe').value;
+
+	importeUno=parseInt(importe);
+
+	importeUno= sueldo*1.25;
+
+	importeConAumento=sueldo+aumento;
+
+	document.getElementById('resultado').value=importeConAumento;
 	
 }
