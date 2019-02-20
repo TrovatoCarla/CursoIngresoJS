5db00d0710cf1260e5475c030b53109d8aefe683
alumno <cntrovato@buenosaires.gob.ar>
2019-02-20 17:38:04 -0300

    Parcial 6 Listo

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/seis.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/seis.js
@@ -1,4 +1,62 @@
 function mostrar()
 {
+var hora;
+var mensaje="";
 
+hora=laHora.value;
+
+
+	switch (hora)
+	{
+		case "6":
+		case "7":
+		case "8":
+		case "9":
+		case "10":
+		case "11":
+
+			mensaje="es de mañana";
+			break;
+
+		case "12":
+		case "13":
+		case "14":
+		case "15":
+		case "16":
+		case "17":
+		case "18":
+		case "19":
+
+			mensaje="es de tarde";
+			break;
+
+		default:
+			mensaje="es de noche";
+			
+
+	}
+
+	if(hora>24)
+	{
+		mensaje="hora no válida";
+	}
+
+	if(hora>19 && hora<25)
+	{	
+		mensaje="a dormir";
+	}
+
+	if(mensaje!="")
+	{	
+		alert(mensaje);
+	}
+
+
+
+
+
+
+
+
+	
 }
