8dfc8c5f23b9b4ecb927e9c41b866beb5073e990
alumno <cntrovato@buenosaires.gob.ar>
2019-02-20 16:53:48 -0300

    Parcial 5

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
@@ -1,4 +1,37 @@
 function mostrar()
 {
+	 var planeta;
+	 var mensaje="";
+
+	 planeta=prompt("Ingrese un planeta ");
+
+	 switch (planeta)
+	{
+		case "tierra":
+			mensaje="acá vivimos";
+			break;
+
+		case "mercurio":
+		case "venus":
+			mensaje="acá hace mas calor";
+			break;
+
+		case "marte":
+		case "jupiter":
+		case "saturno":
+		case "urano":
+		case "neptuno":
+			mensaje="acá hace más frío";
+			break;
+		default:
+			mensaje="no es un planeta";
+			break;
+	}
+
+alert(mensaje);
+
+
+
+
 
 }
