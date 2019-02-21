4ed18a4290e2aca09590c58e9b5ee206c202fe3d
alumno <alumno>
2019-02-20 22:11:37 -0300

    WHILE

--- 5-InstruccionWhile/jsIteraciones (7).js
+++ 5-InstruccionWhile/jsIteraciones (7).js
@@ -5,6 +5,14 @@ function mostrar()
 	var acumulador=0;
 	var respuesta='si';
 
+while(contador=="si")//pongo el maximo 
+{
+	numero=prompt("Ingrese numero");
+	contador=contador+1;//se le suma un literal
+	numero=parseInt(numero);
+	acumulador=acumulador+numero;//se le suma una variable
+	respuesta=prompt("Ingrese si para continuar");
+}
 
 document.getElementById('suma').value=acumulador;
 document.getElementById('promedio').value=acumulador/contador;
