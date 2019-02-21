8a5b7cd1adfc25e423ac1791ca5037521b613769
alumno <alumno>
2019-02-20 22:15:19 -0300

    While8

--- 5-InstruccionWhile/jsIteraciones (8).js
+++ 5-InstruccionWhile/jsIteraciones (8).js
@@ -4,9 +4,26 @@ function mostrar()
 	var contador=0;
 	var positivo=0;
 	var negativo=1;
+	var numero;
 	
 	var respuesta='si';
 
+while(contador=="si")//pongo el maximo 
+{
+	numero=prompt("Ingrese numero");
+	contador=contador+1;//se le suma un literal
+	numero=parseInt(numero);
+	//acumulador=acumulador+numero;//se le suma una variable
+	if(numero<0)
+	{
+		negativo=negativo*numero;
+	}
+	else
+	{
+		positivo=positivo+numero;
+	}
+	
+	respuesta=prompt("Ingrese si para continuar");
 
 document.getElementById('suma').value=positivo;
 document.getElementById('producto').value=negativo;
