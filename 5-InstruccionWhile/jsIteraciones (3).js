770cf87d50fc25291b3b32bc4ddcf9fc5d12fdb4
trovatocarla <cntrovato@buenosaires.gob.ar>
2019-02-23 12:36:08 -0300

    WHILE3

--- 5-InstruccionWhile/jsIteraciones (3).js
+++ 5-InstruccionWhile/jsIteraciones (3).js
@@ -1,7 +1,25 @@
 function mostrar()
 {
-
+var intentos=0;
 var clave = prompt("ingrese el número clave.");
+var flag=false;
+while(clave!="utn750")
+{
+	//alert("clave incorrecta.Reingrese su clave");
+	clave=prompt("Reingrese el numero clave");
+	intentos=intentos+1;
+	if(intentos==3)
+	{
+		flag=true;
+		break;//el break aca rompe la estructura repetitiva.Cuando se cumple la condicion me saca de la estructura
+	}
+}
+	if(flag==false)
+	{
+		alert("Bienvenido al sistema");
+	}
+
+	
 
 
 }//FIN DE LA FUNCIÓN
