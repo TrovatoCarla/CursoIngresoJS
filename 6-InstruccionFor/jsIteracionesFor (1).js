8d4c475eb4f4649fe42f13a5555869a7cb2dca6a
alumno <alumno>
2019-02-25 21:54:52 -0300

    For

--- 6-InstruccionFor/jsIteracionesFor (1).js
+++ 6-InstruccionFor/jsIteracionesFor (1).js
@@ -2,7 +2,8 @@ function mostrar()
 {	
 	var contador=0
 	
-	for(;contador<10;)
+	for(;contador<10;)// para que funcione no necesariamente tene q estar la primer y ultima parte
+		//la condicion puede no tener nada que ver con el contador
 	{
 
 
--- 6-InstruccionFor/jsIteracionesFor (3).js
+++ 6-InstruccionFor/jsIteracionesFor (3).js
@@ -1,8 +1,33 @@
 function mostrar()
 {
 
-var repetciones = prompt("ingrese el número de repeticiones");
+//var repetciones = prompt("ingrese el número de repeticiones");
 
+	var numeroIngresado;
+	var numeroPerfecto;
+	var acumulador;
+
+	numeroIngresado=prompt("Ingrese numero");
+	numeroIngresado=parseInt(numeroIngresado);
+
+	for(recorrido=numeroIngresado-1;recorrido>1;recorrido--)// este recorre todos los numeros!
+	{		
+		for(numeroAnterior=recorrido-1;numeroAnterior>0;numeroAnterior--)// este me saca el numero perfecto!
+		{
+			//console.log(numeroAnterior);
+			if(numeroIngresado% numeroAnterior==0)
+			{
+				acumulador=acumulador+numeroAnterior;
+			}
+
+		}
+
+		if(numeroIngresado==acumulador)
+		 {
+	 		console.log("Es perfecto" +numeroIngresado);
+		 }
+		
+	}
 
 
 }//FIN DE LA FUNCIÓN
